29e997ab32ed53f42e0dc1bda25c556ae7ac38ec
Ed Lee <eddieberklee@gmail.com>
2014-03-13 06:31:12 -0700

    new init

--- app.rb
+++ app.rb
@@ -1,7 +1,23 @@
 require 'sinatra'
+require 'haml'
+require 'sass'
+require 'compass'
 
-get '/' do
-  haml :index, :format => :html5
+configure do
+	Compass.configuration do |config|
+		config.project_path = File.dirname(__FILE__)
+		config.sass_dir = 'public/css'
+	end
+	set :haml, { :format => :html5 }
+	set :sass, Compass.sass_engine_options
+	set :scss, Compass.sass_engine_options
 end
 
+get '/' do
+  haml :index
+end
 
+get '/css/:name.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass(:"../public/css/#{params[:name]}", Compass.sass_engine_options )
+end
